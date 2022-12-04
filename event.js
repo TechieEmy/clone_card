4aee9e8f84f31db0db786d6416ede1357283d973
emily <ogwuche1998@gmail.com>
2022-12-04 19:05:54 +0100

    js commit

---
+++ event.js
@@ -0,0 +1,13 @@
+const click = document
+  .getElementById("click")
+  .addEventListener("click", buttonClick);
+
+function buttonClick(e) {
+let text = document.getElementById('click');
+if(text.textContent = 'follow'){
+                    return text.textContent = 'following'
+}
+                    return text.textContent = 'follow'
+}
+
+ 
